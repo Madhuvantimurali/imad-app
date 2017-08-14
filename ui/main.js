7577fb474a6636409d39228b286acdd34a79b2e6
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-08-15 00:25:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,13 @@ var element = document.getElementById('main-text');
 element.innerHTML='Madhu is working';
 //move an image
 var img = document.getElementById('madi');
-img.onclick = function(){
-    img.style.marginLeft = '100px';
+var marginLeft =0;
+
+function moveRight(){
+    marginLeft= marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
     
+}
+img.onclick = function(){
+    var interval = setInterval(moveRight,50);
 };
