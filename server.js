18137ec8046e7c43ae0f84e149bca1e28331620b
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-08-14 23:08:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var content={
-    
-}
 
 var article={
     'article-two':{
