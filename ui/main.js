9f89ced203cac87b69ec310a3870920c83977b90
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-08-15 00:57:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,6 @@
 console.log('Loaded!');
-alert("HI, javaSCRIPT is working");
-var element = document.getElementById('main-text');
-element.innerHTML='Madhu is working';
+/*var element = document.getElementById('main-text');
+element.innerHTML='Madhu is working';*/
 //move an image
 var img = document.getElementById('madi');
 var marginLeft =0;
