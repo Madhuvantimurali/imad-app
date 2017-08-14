58a4687f5196fd4410b55063fb2895a9e52497ae
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-08-15 01:12:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ function createTemplate(data){
                `;
     return htmlTemplate;
 }
+var counter = 0;
 app.get('/counter',function(req,res){
    counter = counter + 1;
    res.send(counter.toString());
