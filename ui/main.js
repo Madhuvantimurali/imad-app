488fdc42c3e342c37af81cd53ee2293bd037d75e
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-08-15 15:06:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,13 @@ button.onclick = function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readystate === XMLHttpRequest.DONE){
+            console.log("ready");
             if(request.status === 200){
+                console.log("init span and count")
                 var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
-                
+                console.log("finished");
             }
         }
     };
