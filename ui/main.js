c9d8b5b9c6cae4d811b9fa71f52ba7d2892fe269
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-08-15 21:07:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,15 +20,31 @@ button.onclick = function(){
 var nameInput= document.getElementById('name');
 var name = nameInput.value;
 var submit= document.getElementById('submit_btn');
-submit.onclick = function(){
+submit.onclick = function()
+{
   
-  //make request to server
+  var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                var names=['name1', 'name2', 'name3', 'name4'];
+                var list=' ';
+                for(i=0;i<names.length;i++)
+                {
+                list += '<li>'+ names[i] +'</li>';
+                }
+            }
+        }
+    };
+    request.open('GET','http://madhuvantimuralik.imad.hasura-app.io/submit-name?=name=' + name,true);
+    request.send(null);
+};        
   
   // capture name and display as a list
-  var names=['name1', 'name2', 'name3', 'name4'];
-  var list=' ';
-  for(i=0;i<names.length;i++){
-      list += '<li>'+ names[i] +'</li>';
+  
   }
   var ul = document.getElementById('namelist');
   ul.innerHTML=list;
