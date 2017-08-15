bbd46292c596a351c5879da65c152f426915561f
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-08-15 20:42:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,21 @@ button.onclick = function(){
     request.open('GET','http://madhuvantimuralik.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+// submit name
+var nameInput= document.getElementById('name');
+var name = nameInput.value;
+var submit= document.getElementById('submit_btn');
+submit.onclick = function(){
+  
+  //make request to server
+  
+  // capture name and display as a list
+  var names=['name1', 'name2', 'name3', 'name4'];
+  var list=' ';
+  for(i=0;i<names.length;i++){
+      list += '<li>'+ names[i] +'</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML=list;
+  };
