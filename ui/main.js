a0b0a12c914ea62199f6581cc06c883f60cee231
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-08-15 20:28:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,19 +2,13 @@
 console.log("im here");
 var button = document.getElementById('counter');
 button.onclick = function(){
-    console.log("at onclick event")
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
-        console.log("State change")
         if(request.readyState === XMLHttpRequest.DONE){
-            console.log("Checking if done");
             if(request.status === 200){
                 var counter = request.responseText;
-                //counter = counter + 1;
-                console.log(counter);
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
-                console.log(span);
             }
         }
     };
