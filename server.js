78257309a529a45f080a6f6d62749864949fa761
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-08-29 19:29:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,10 +92,23 @@ function createTemplate(data){
     return htmlTemplate;
 }
 
-app.get('/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
   var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));   
-    });
+  pool.query("SELECT * FROM article WHERE title ='"+ articleName+"'",function(err,result){
+     if(err){
+         result.status(500).send(err.toString());
+     } 
+     else{
+         if(result.rows.length === 0){ res.status(404).send('Article not found');
+           }
+         else{
+         var articleData = result.rows[0];
+         res.send(createTemplate(articles[articleName])); 
+          }
+    } 
+  });
+    
+});
 
     
 app.get('/ui/style.css', function (req, res) {
