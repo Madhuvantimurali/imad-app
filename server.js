7ec072e888dfb9c5471c047f2ba37b9951c0aa7b
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-08-29 18:43:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,9 +19,8 @@ app.get('/', function (req, res) {
 
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
-    pool.query('SELECT * from test',function(err,result){
+    pool.query('SELECT * FROM test',function(err,result){
         if(err){
-            console.log('error occuring');
             res.status(500).send(err.toString());
         }
         else
