62fdbebc862051f8bcedba02829af334bce52de2
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-09-06 16:40:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,6 +66,7 @@ app.post('/create-user',function(req,res){
 app.post('/login',function(req,res){
 var username = req.body.username;
    var password = req.body.password;
+   res.send(JSON.stringify(password));
    
    pool.query('SELECT * FROM "user" WHERE username=$1',[username],function(err,result){
        if(err){
