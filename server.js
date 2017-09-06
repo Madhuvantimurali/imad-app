50fc29841224ceac04304ad1d5f8d3ff7f1a52f8
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-09-06 15:08:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post('/create-user',function(req,res){
         }
         else
         {
-            res.send(JSON.stringify(result.rows));
+            res.send("User created successfully"+username);
         }
    });
 });
