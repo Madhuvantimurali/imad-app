9c28d527ea4762179700473edda5466796c01af3
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-09-06 19:59:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,4 @@
-//counter code
-var button = document.getElementById('counter');
-button.onclick = function(){
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    request.open('GET','http://madhuvantimuralik.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-
-// submit name
-
+// submit username and password
 var submit= document.getElementById('submit_btn');
 submit.onclick = function()
 {
@@ -25,20 +7,20 @@ submit.onclick = function()
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             if(request.status === 200){
-                var names=request.responseText;
-                names = JSON.parse(names);
-                var list=' ';
-                for(i=0;i<names.length;i++){
-                list += '<li>'+ names[i] +'</li>';
-                }
-               var ul = document.getElementById('namelist');
-               ul.innerHTML=list;
+               console.log('user logged in successfully');
+               alert('Login Successful');
             }
+            else if(request.status === 403){
+                alert('Username/password is incorrect');
+            }
+            else if(request.status === 500){
+                alert('Something went wrong with the server');
         }    
+     }
     };
-    var nameInput= document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET','http://madhuvantimuralik.imad.hasura-app.io/submit-name?name=' + name,true);
+    var username= document.getElementById('username').value;
+    var password= document.getElementById('password').value;
+    request.open('POST','http://madhuvantimuralik.imad.hasura-app.io/login',true);
     request.send(null);
 };        
   
