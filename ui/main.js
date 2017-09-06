26393972c865d0f58298de9bdc37f5bd590b64d1
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-09-06 21:41:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,25 @@
+//registration for user
+var register = document.getElementById('reg_page');
+register.onclick=function()
+{
+  var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+               console.log('at registration');
+               alert('Welcome to Registration');
+            }
+            else if(request.status === 500){
+                alert('Something went wrong with the server');
+        }    
+     }
+  
+};
+    var username= document.getElementById('username').value;
+    var password= document.getElementById('password').value;
+    request.open('POST','http://madhuvantimuralik.imad.hasura-app.io/create-user',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username,password: password}));
 // submit username and password
 var submit= document.getElementById('submit_btn');
 submit.onclick = function()
