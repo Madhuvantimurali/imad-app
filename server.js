00ff5fb55436e01225215da2d05118e1e363abf6
Madhuvantimurali <madhuvanti.muralik@gmail.com>
2017-09-06 16:00:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,14 +77,14 @@ var username = req.body.username;
                 res.status(403).send('username/password is inavlid');
             }
             else{
-            var dbString =result.rows[0].password;
+            var dbString = result.rows[0].password;
             var salt= dbString.split('$')[2];
             var hashedPassword = hash(password,salt);
             if(hashedPassword === dbString){
              res.send("Credentials are correct");   
             }
             else{
-            res.status(403).send("Username or Password is invalid");    
+            res.status(403).send("Invalid "+ salt );    
             }
             }
         }
